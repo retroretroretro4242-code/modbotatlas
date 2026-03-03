acb5142b8659fbafe053c7c6210a42774a8563bc
retroretroretro4242-code <retro.retro.retro.42.42@gmail.com>
2026-03-03 13:37:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from datetime import timedelta
 import threading
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 if not TOKEN:
     raise Exception("DISCORD_TOKEN environment variable not set.")
